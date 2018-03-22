79a0a1711212370a5d15210eeb3b399a3c346934
Rodolfo Valdivieso <valdivieso_rodolfo@hotmail.com>
2018-03-22 12:29:15 -0500

    Assignment Hypotenuse

---
+++ javafun/Pythagorean.java
@@ -0,0 +1,9 @@
+public class Pythagorean {
+    public double calculateHypotenuse(int legA, int legB) {
+        // the hypotenuse is the side across the right angle. 
+        // calculate the value of c given legA and legB
+        double result = Math.sqrt(((legA * legA) + (legB * legB)));
+        return result;
+
+    }
+}
---
+++ javafun/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Pythagorean iD = new Pythagorean();
+        double h = iD.calculateHypotenuse(3, 4);
+        System.out.println(h);
+    }
+}
