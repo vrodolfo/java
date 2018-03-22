34574e09ce1013e703d80d3f50b8be66a5bb844e
Rodolfo Valdivieso <valdivieso_rodolfo@hotmail.com>
2018-03-22 10:01:59 -0500

    First assignment

---
+++ javafun/Rodolfo.java
@@ -0,0 +1,7 @@
+public class Rodolfo{
+	public static void main(String[] args){
+		System.out.println("My Names is Rodolfo");
+		System.out.println("I'm 100 years old");
+		System.out.println("I live in Texas");
+	}
+}
