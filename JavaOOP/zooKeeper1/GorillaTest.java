bce5c8952fc6e036aa2f30201fed84af2ab99b2e
Rodolfo Valdivieso <valdivieso_rodolfo@hotmail.com>
2018-03-23 14:41:43 -0500

    Assignmant Java OOP Inheritance

---
+++ JavaOOP/zooKeeper1/GorillaTest.java
@@ -0,0 +1,17 @@
+class GorillaTest {
+    public static void main(String args[]) {
+        Gorilla g = new Gorilla();
+        g.throwSomething();
+        g.displayEnergy();
+        g.throwSomething();
+        g.displayEnergy();
+        g.throwSomething();
+        g.displayEnergy();
+        g.eatBananas();
+        g.displayEnergy();
+        g.eatBananas();
+        g.displayEnergy();
+        g.climb();
+        g.displayEnergy();
+    }
+}
